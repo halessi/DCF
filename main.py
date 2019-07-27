85df7f63c5fee5e87410f69dac42aea222abb536
Hugh Alessi <hughalessi@gmail.com>
2019-07-27 15:22:35 -0400

    clean up terminal output for  FCF

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import argparse
 from decimal import Decimal
 
-from data import *
+from statements import *
 
 def ulFCF(ebit, tax_rate, non_cash_charges, cwc, cap_ex):
     '''
@@ -31,7 +31,7 @@ def get_discount_rate():
     '''
     return .1 # TODO: implement 
 
-def forecast_flows(ticker, period, growth_rate):
+def forecast_flows(ticker, period, earnings_growth_rate, cap_ex_growth_rate):
     '''
     Forecast free cash flows _period_ years into future.
 
@@ -60,27 +60,28 @@ def forecast_flows(ticker, period, growth_rate):
     flows = []
 
     # Now let's iterate through years, starting with most recent year
-    print('\nForecasting flows for {} years out, starting with at date {} with growth {}.'.format(period, income_statement[0]['date'], growth_rate))  
-    print('| DFCF |  EBIT  |  D&A  |  CWC  |  CAP_EX  | ')
+    print('\nForecasting flows for {} years out, starting with at date {} with earnings growth {}.'.format(period, income_statement[0]['date'], earnings_growth_rate))  
+    print('         DFCF   |    EBIT   |    D&A    |    CWC    |   CAP_EX   | ')
     for yr in range(1, period+1):    
 
         # increment each value by growth rate
-        ebit = ebit * (1 + growth_rate)
-        non_cash_charges = non_cash_charges * (1 + growth_rate)
+        ebit = ebit * (1 + earnings_growth_rate)
+        non_cash_charges = non_cash_charges * (1 + earnings_growth_rate)
         cwc = cwc * 0.9                             # TODO: evaluate this cwc rate? 0.1 annually?
-        cap_ex = cap_ex * (1.045)        
+        cap_ex = cap_ex * (1 + cap_ex_growth_rate)         
 
         flow = ulFCF(ebit, tax_rate, non_cash_charges, cwc, cap_ex)
         discounted_flow = flow/(1 + discount)**yr
         flows.append(discounted_flow)
 
-        print(str('%.3E' % discounted_flow) + ' | ',
-              str('%.3E' & ebit) + ' | ',
-              str('%.3E' % non_cash_charges) + ' | ', 
-              str('%.3E' % cwc) + ' | ',
-              str('%.3E' % cap_ex) + ' | ')
+        print(str(int(income_statement[0]['date'][0:4]) + yr) + '  ',
+              '%.2E' % Decimal(discounted_flow) + ' | ',
+              '%.2E' % Decimal(ebit) + ' | ',
+              '%.2E' % Decimal(non_cash_charges) + ' | ',
+              '%.2E' % Decimal(cwc) + ' | ',
+              '%.2E' % Decimal(cap_ex) + ' | ')
 
-    return sum(flows)
+    return sum(flows) 
 
 
 def main(args):
@@ -88,7 +89,7 @@ def main(args):
     a very basic 2-stage DCF implemented for learning purposes
     '''
     if args.ticker is not None:
-        sum_of_present_values = forecast_flows(args.ticker, args.period, args.growth_rate)
+        sum_of_present_values = forecast_flows(args.ticker, args.period, args.earnings_growth_rate, args.cap_ex_growth_rate)
     else:
         raise ValueError('Must specify a ticker.')
 
@@ -99,7 +100,8 @@ if __name__ == '__main__':
 
     parser.add_argument('-p', '--period', help = 'years to forecast', type = int, default =  5)
     parser.add_argument('-t', '--ticker', help = 'ticker of company', type = str)
-    parser.add_argument('-g', '--growth_rate', help = 'growth in revenue, YoY',  type = float, default = .05)
+    parser.add_argument('-eg', '--earnings_growth_rate', help = 'growth in revenue, YoY',  type = float, default = .05)
+    parser.add_argument('-cg', '--cap_ex_growth_rate', help = 'growth in cap_ex, YoY', type = float, default = 0.045)
 
     args = parser.parse_args()
     main(args)
