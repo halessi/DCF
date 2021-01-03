c70eea873185afb09e71129e25f3a625c0776dd3
TheSnoozer <s.lange@alumni.tu-berlin.de>
2021-01-03 18:23:02 +0100

    make code more pythonic

--- modeling/data.py
+++ modeling/data.py
@@ -1,15 +1,16 @@
-'''
+"""
 Utilizing financialmodelingprep.com for their free-endpoint API
 to gather company financials.
 
 NOTE: Some code taken directly from their documentation. See: https://financialmodelingprep.com/developer/docs/. 
-'''
+"""
 
 from urllib.request import urlopen
 import json
 
+
 def get_jsonparsed_data(url):
-    '''
+    """
     Fetch url, return parsed json. 
 
     args:
@@ -17,29 +18,31 @@ def get_jsonparsed_data(url):
     
     returns:
         parsed json
-    '''
+    """
     response = urlopen(url)
     data = response.read().decode('utf-8')
     return json.loads(data)
 
+
 def get_EV_statement(ticker, period = 'annual'):
-    '''
+    """
     Fetch EV statement, with details like total shares outstanding, from FMP.com
 
     args:
         ticker: company tickerr
     returns:
         parsed EV statement
-    '''
+    """
     if period == 'annual':
         url = 'https://financialmodelingprep.com/api/v3/enterprise-value/{}'.format(ticker)
     elif period == 'quarter':
         url = 'https://financialmodelingprep.com/api/v3/enterprise-value/{}?period=quarter'.format(ticker)
     return get_jsonparsed_data(url)
 
+
 #! TODO: maybe combine these with argument flag for which statement, seems pretty redundant tbh
 def get_income_statement(ticker, period = 'annual'):
-    '''
+    """
     Fetch income statement.
 
     args:
@@ -48,7 +51,7 @@ def get_income_statement(ticker, period = 'annual'):
 
     returns:
         parsed company's income statement
-    '''
+    """
     if period == 'annual':
         url = 'https://financialmodelingprep.com/api/v3/financials/income-statement/{}'.format(ticker)
     elif period == 'quarter':
@@ -60,7 +63,7 @@ def get_income_statement(ticker, period = 'annual'):
 
 
 def get_cashflow_statement(ticker, period = 'annual'):
-    '''
+    """
     Fetch cashflow statement.
 
     args:
@@ -69,7 +72,7 @@ def get_cashflow_statement(ticker, period = 'annual'):
 
     returns:
         parsed company's cashflow statement
-    '''
+    """
     if period == 'annual':
         url = 'https://financialmodelingprep.com/api/v3/financials/cash-flow-statement/{}'.format(ticker)
     elif period == 'quarter':
@@ -79,8 +82,9 @@ def get_cashflow_statement(ticker, period = 'annual'):
 
     return get_jsonparsed_data(url)
 
+
 def get_balance_statement(ticker, period = 'annual'):
-    '''
+    """
     Fetch balance sheet statement.
 
     args:
@@ -89,7 +93,7 @@ def get_balance_statement(ticker, period = 'annual'):
 
     returns:
         parsed company's balance sheet statement
-    '''
+    """
     if period == 'annual':
         url = 'https://financialmodelingprep.com/api/v3/financials/balance-sheet-statement/{}'.format(ticker)
     elif period == 'quarter':
@@ -99,8 +103,9 @@ def get_balance_statement(ticker, period = 'annual'):
 
     return get_jsonparsed_data(url)
 
+
 def get_stock_price(ticker):
-    '''
+    """
     Fetches the stock price for a ticker
 
     args:
@@ -108,12 +113,13 @@ def get_stock_price(ticker):
     
     returns:
         {'symbol': ticker, 'price': price}
-    '''
+    """
     url = 'https://financialmodelingprep.com/api/v3/stock/real-time-price/{}'.format(ticker)
     return get_jsonparsed_data(url)
 
+
 def get_batch_stock_prices(tickers):
-    '''
+    """
     Fetch the stock prices for a list of tickers.
 
     args:
@@ -121,15 +127,16 @@ def get_batch_stock_prices(tickers):
     
     returns:
         dict of {'ticker':  price}
-    '''
+    """
     prices = {}
     for ticker in tickers:
         prices[ticker] = get_stock_price(ticker)['price']
 
     return prices
 
+
 def get_historical_share_prices(ticker, dates):
-    '''
+    """
     Fetch the stock price for a ticker at the dates listed.
 
     args:
@@ -138,7 +145,7 @@ def get_historical_share_prices(ticker, dates):
 
     returns:
         {'date': price, ...}
-    '''
+    """
     prices = {}
     for date in dates:
         date_start, date_end = date[0:8] + str(int(date[8:]) - 2), date
@@ -154,8 +161,9 @@ def get_historical_share_prices(ticker, dates):
 
     return prices
 
+
 if __name__ == '__main__':
-    ''' quick test, to use run data.py directly '''
+    """ quick test, to use run data.py directly """
 
     ticker = 'AAPL'
     data = get_cashflow_statement(ticker)
